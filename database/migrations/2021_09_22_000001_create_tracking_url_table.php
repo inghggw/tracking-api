35a3a631708e0c26e5b8d7cbfa0a8ae4d4eafe50
Giovanny Gonzalez <henrygiovanny.gonzalezwaltero@mercadolibre.com.co>
2021-09-23 21:30:41 -0500

    migrations

---
+++ database/migrations/2021_09_22_000001_create_tracking_url_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrackingUrlTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracking_url', function (Blueprint $table) {
+            $table->id();
+            $table->string('hash', 7)->unique();
+            $table->bigInteger('opens')->default(0);
+            $table->text('url');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracking_url');
+    }
+}
